826db65108afdce7a3f32777e7558f395714f7e2
Peralta <lm.peralta184@gmail.com>
2021-06-29 19:48:08 -0300

    commit inicial

---
+++ Bolo.mk
@@ -0,0 +1,22 @@
+# Bolo de Trigo :cake:
+
+### Ingredientes:
+
+- 2 xícaras(chá) de açucar
+- 3 xícaras (chá) de farinha de trigo
+- 4 colheres (sopa) de margarina
+- 3 ovos
+- 1 e 1/2 xicara (chá) de leite
+- 1 colher (sopa) bem cheia de fermento em pó
+
+### Preparo
+
+1. Bata as claras em neve e reserve
+2. Misture as gemas, a margarina e o açúcar até obter uma massa homogênea
+3. Acrescente o leite e a farinha de trigo aos poucos, sem parar de bater
+4. Por último, adicione as claras em neve e o fermento
+5. Despeje a massa em uma forma grande de furo central untada e enfarinhada
+6. Asse em forno médio 180ºC, preaquecido, por aproximadamente 40 minutos
+
+
+
